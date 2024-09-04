df627441f890f9f504d512f594fd9974e7e4e9f2
jknabe1 <jens.knabe@kkrecords.se>
2024-09-04 10:29:32 +0200

    chore: Add NotFound component and API route for 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function NotFound() {
+  return (
+    <div className="flex items-center justify-center h-screen flex-col">
+      <h1 className="md:text-4xl text-center sm:text-2xl underline">Aj då, 404 - Byt diskett</h1>
+    </div>
+      );
+}
