3a0d503e4bd0ec95f26fa7a1629d9da24dfb18b0
jknabe1 <jens.knabe@kkrecords.se>
2024-09-11 21:37:22 +0200

    chore: Update page layout and add underlined style to links

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,51 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Music For Pennies",
+  title: {
+    template: '%s | Music For Pennies',
+    default: 'Music For Pennies', 
+  },
   description: "Music For Pennies",
+
+  metadataBase: new URL('https://musicforpennies.se'),
+  
+  openGraph: {
+    title: 'Music For Pennies',
+    description: "Music For Pennies",
+    url: 'https://musicforpennies.se',
+    siteName: 'Music For Pennies',
+    images: [
+      {
+        url: 'https://musicforpennies.se/api', // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: 'sv_SE',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+  icons: {
+    icon: '/icon.png',
+    shortcut: '/shortcut-icon.png',
+    apple: '/apple-icon.png',
+    other: {
+      rel: 'apple-touch-icon-precomposed',
+      url: '/apple-touch-icon-precomposed.png',
+    },
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'wwww.musicforpennies.se',
+    description: "Music For Pennies",
+    images: ['https://musicforpennies.se/api'], // Must be an absolute URL
+  },
 };
+
+
 
 export default function RootLayout({
   children,
